d499422c114df50264903b59c1171008dc7bdb87
Tim Helmstedt <tim.helmstedt@gmail.com>
2017-12-21 11:42:03 +1000

    Trayer aligns from left, fixes display issues on multi monitor

--- xmonad/.xmonad/bin/startup.sh
+++ xmonad/.xmonad/bin/startup.sh
@@ -6,7 +6,7 @@ killall trayer
 conky -c /home/tim/.xmonad/bin/conkydesktop &
 conky -c /home/tim/.xmonad/bin/conkytop &
 
-trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand false --width 200 --widthtype pixel \
-    --transparent true --tint 0x000000 --alpha 0 --height 24  & 
+trayer --edge top --align left --SetDockType true --SetPartialStrut true --expand false --width 200 --widthtype pixel \
+    --transparent true --tint 0x000000 --alpha 0 --height 24  --margin 1720 &
 
 #setxkbmap -option 'ctrl:nocaps'
