77b72e1019ab68fbdbfacd7ed6771bd4c6f9d6ee
Tim Helmstedt <tim.helmstedt@gmail.com>
2018-03-05 12:44:45 +1000

    conky icons restructure

--- xmonad/.config/conky/startup.sh
+++ xmonad/.config/conky/startup.sh
@@ -3,8 +3,11 @@
 killall conky
 killall trayer
 
-conky -c /home/tim/.config/conky/conkydesktop &
-conky -c /home/tim/.config/conky/conkytop &
+IFACE=$(awk 'NR>1{print $6}' < /proc/net/arp | head -n1)
+cat /home/tim/.config/conky/conkydesktop | sed "s/NETWORK_INTERFACE/$IFACE/g" > ~/.cache/conkydesktop
+cat /home/tim/.config/conky/conkytop | sed "s/NETWORK_INTERFACE/$IFACE/g" > ~/.cache/conkytop
+conky -c ~/.cache/conkydesktop &
+conky -c ~/.cache/conkytop &
 
 trayer --edge top --align left --SetDockType true --SetPartialStrut true --expand false --width 200 --widthtype pixel \
     --transparent true --tint 0x000000 --alpha 0 --height 24  --margin 1720 &
