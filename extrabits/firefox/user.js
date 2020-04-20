590422d7acb83634bb8cd17f2df9bdc376dc431c
Tim Helmstedt <tim.helmstedt@gmail.com>
2020-04-21 09:48:23 +1000

    no autoplay for firefox

--- extrabits/firefox/user.js
+++ extrabits/firefox/user.js
@@ -1,3 +1,5 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.link.open_newwindow.restriction", 0);
+user_pref("media.autoplay.default", 1);
+user_pref("media.autoplay.allow-muted", 1);
 
