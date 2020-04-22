f53003cc50cafced88da9e2ec9b200c2f5364c94
Tim Helmstedt <tim.helmstedt@gmail.com>
2020-04-22 15:08:11 +1000

    dac switching fixes

--- bin/bin/switch_pulse_sinks.sh
+++ bin/bin/switch_pulse_sinks.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-SPEAKERS="alsa_output.usb-SMSL_AUDIO_SMSL_AD18_AMP-00.analog-stereo"
-HEADPHONES="alsa_output.usb-Grace_Design_SDAC-00.analog-stereo"
+SPEAKERS="alsa_output.usb-SMSL_AUDIO_SMSL_AD18_AMP-00.iec958-stereo"
+HEADPHONES="alsa_output.usb-Grace_Design_SDAC-00.iec958-stereo"
 
 CURRENT=$(pactl list short sink-inputs | head -n 1 | awk '{print $2}')
-CURRENT_NAME=$(pactl list short sinks | grep "^${CURRENT}.*" | awk '{print $2}')
+CURRENT_NAME=$(pactl list short sinks | grep "^${CURRENT}[^0-9]" | awk '{print $2}')
 
 IDX=$SPEAKERS
-if [ "$CURRENT_NAME" == "$SPEAKERS" ]; then
+if [[ "$CURRENT_NAME" == "$SPEAKERS" ]]; then
   IDX=$HEADPHONES
 fi
 
-pacmd set-default-sink "$IDX"
+  pacmd set-default-sink "$IDX"
 for input_idx in $(pactl list short sink-inputs | awk '{print $1}'); do
-   pactl move-sink-input "$input_idx" "$IDX"
+  pactl move-sink-input "$input_idx" "$IDX"
 done
