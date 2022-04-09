5b1a0a9024f8b5407cd06440761abd84ddd3641c
Tim Helmstedt <tim.helmstedt@gmail.com>
2022-04-09 10:38:39 +1000

    show pulse sink and correct temps in polybar

--- x11/.config/polybar/cpuinfo.sh
+++ x11/.config/polybar/cpuinfo.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 FREQ=$(cat /proc/cpuinfo | grep MHz | awk -F':' '{print $2}' | awk -F'.' '{ total += $1; count++} END { print total/count }')
-TEMP=$(sensors | grep "Tdie" | awk '{print $2}' | sed 's/+//g')
+TEMP=$(sensors | grep "Tctl" | awk '{print $2}' | sed 's/+//g')
+FANSPEED=$(sensors | grep "fan2" | awk '{print $2}' | sed 's/+//g')
 #USAGE=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print int(usage)}')
 USAGE=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print int(100 - $1)}')
 
-echo $USAGE% ${FREQ}MHz ${TEMP}
+echo $USAGE% ${FREQ}MHz ${TEMP} ${FANSPEED}RPM
