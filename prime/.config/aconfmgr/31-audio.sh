3cb9ddcde872da6260ff4f0ae9bbdc4c2e8c5d1c
Tim Helmstedt <tim.helmstedt@gmail.com>
2022-05-27 10:07:40 +1000

    migrated from pulseaudio to pipewire

--- prime/.config/aconfmgr/31-audio.sh
+++ prime/.config/aconfmgr/31-audio.sh
@@ -1,11 +1,12 @@
 # Packages
 AddPackage a2jmidid # A daemon for exposing legacy ALSA sequencer applications in JACK MIDI system.
 AddPackage alsa-utils # Advanced Linux Sound Architecture - Utilities
-AddPackage jack2 # C++ version of the JACK low-latency audio server for multi-processor machines
 AddPackage paprefs # Configuration dialog for PulseAudio
 AddPackage pasystray # PulseAudio system tray (a replacement for padevchooser)
 AddPackage pavucontrol # PulseAudio Volume Control
-AddPackage pulseaudio # A featureful, general-purpose sound server
+AddPackage pipewire-alsa # Low-latency audio/video router and processor - ALSA configuration
+AddPackage pipewire-jack # Low-latency audio/video router and processor - JACK support
+AddPackage pipewire-pulse # Low-latency audio/video router and processor - PulseAudio replacement
 AddPackage qjackctl # A Qt front-end for the JACK low-latency audio server
 AddPackage realtime-privileges # Realtime privileges for users
 
@@ -13,8 +14,7 @@ AddPackage realtime-privileges # Realtime privileges for users
 AddPackage --foreign vcvrack-bin # Open-source virtual modular synthesizer
 
 # Files
-CopyFile /etc/pulse/daemon.conf
-CopyFile /etc/pulse/default.pa
-
-# Services
-CreateLink /etc/systemd/user/sockets.target.wants/pulseaudio.socket /usr/lib/systemd/user/pulseaudio.socket
+CreateLink /etc/systemd/user/pipewire.service.wants/pipewire-media-session.service /usr/lib/systemd/user/pipewire-media-session.service
+CreateLink /etc/systemd/user/pipewire-session-manager.service /usr/lib/systemd/user/pipewire-media-session.service
+CreateLink /etc/systemd/user/sockets.target.wants/pipewire-pulse.socket /usr/lib/systemd/user/pipewire-pulse.socket
+CreateLink /etc/systemd/user/sockets.target.wants/pipewire.socket /usr/lib/systemd/user/pipewire.socket
