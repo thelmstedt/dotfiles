3e45f2b51240f1db3ccbb2889a6f660a204855d2
Tim Helmstedt <tim.helmstedt@gmail.com>
2026-02-18 08:38:21 +1000

    better waybar styling

--- wayland/.config/waybar/kvm-status.sh
+++ wayland/.config/waybar/kvm-status.sh
@@ -2,6 +2,9 @@
 
 if lsusb | grep -qE '04d9:4545|046d:c52b'; then
   echo '{"text": "", "class": "connected", "tooltip": "kvm: local"}'
-else
-  echo '{"text": "LAP", "class": "disconnected", "tooltip": "kvm: remote"}'
+  exit
 fi
+
+cat <<EOF
+{"text": "<span background='red' color='#1a1b26'>LAP</span>", "class": "disconnected", "tooltip": "kvm: remote"}
+EOF
